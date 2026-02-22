05b2262259ce798ac6ad1506bc9add5c78b5162b
ubaid shekh <ubaidjmi2022@gmail.com>
2026-02-22 11:31:21 +0530

    update

--- routes/createuser.js
+++ routes/createuser.js
@@ -4,15 +4,18 @@ const User = require("../Modal/User");
 createuserRouter.post("/", async (req, res) => {
   try {
     console.log("recieved request");
-    const { employeeId, password } = req.body;
+    const { name, employeeId, email, number, password } = req.body;
     console.log(`name: ${name},
             employeeId: ${employeeId},
-          
+            email: ${email},
+            number: ${number},
             password: ${password},`);
 
     const myformData = new User({
+      fullName: name,
       employeeId: employeeId,
-
+      email: email,
+      phone: number,
       password: password,
     });
     myformData.save();
