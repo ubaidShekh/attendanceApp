0c00b2fcf9c635527c08aac3732383238790cbe0
ubaid shekh <ubaidjmi2022@gmail.com>
2026-03-08 00:56:21 +0530

    update

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ app.use((req, res, next) => {
 app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
